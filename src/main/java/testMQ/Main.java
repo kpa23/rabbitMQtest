e30f40172add62953c7c1cf2955ea8eea66dd058
kpa23 <kpa23.root@gmail.com>
2023-01-26 22:12:42 +0300

    test produce\consume

---
+++ src/main/java/testMQ/Main.java
@@ -0,0 +1,76 @@
+package testMQ;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Main {
+    private final static String QUEUE_NAME = "order_queue";
+    private final static String HOST = "localhost";
+    private final static int PORT = 5672;
+
+    public static void produce() {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(HOST);
+        factory.setPort(PORT);
+        try {
+            Channel channel = factory.newConnection().createChannel();
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+            // produce 10 messages
+            for (int i = 0; i < 10; i++) {
+                String message = "{\"oderId\":" + ThreadLocalRandom.current().nextInt(
+                        1000, 2000) + ", \"items\":[{\"id\":" + ThreadLocalRandom.current().nextInt(
+                        1000, 2000) + "},{\"id\":" + ThreadLocalRandom.current().nextInt(
+                        1, 10) + "}]}";
+
+                channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+                System.out.println("Produced: " + message);
+            }
+            channel.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void consume() {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(HOST);
+        factory.setPort(PORT);
+        try {
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+
+            DefaultConsumer consumer = new DefaultConsumer(channel) {
+                @Override
+                public void handleDelivery(
+                        String consumerTag,
+                        Envelope envelope,
+                        AMQP.BasicProperties properties,
+                        byte[] body) throws IOException {
+
+                    String message = new String(body, StandardCharsets.UTF_8);
+                    System.out.println("Consumed: " + message);
+                }
+            };
+            channel.basicConsume(QUEUE_NAME, true, consumer);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            produce();
+            consume();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
